0575865881e836e48bb1d48883d097a23c4b7f1f
Magda <magdalenar@onet.com.pl>
2021-12-13 00:29:01 +0100

    implemented testing

--- homework.py
+++ homework.py
@@ -2,6 +2,8 @@ import argparse
 import json
 import os
 from typing import List, Union
+import pytest
+from pytest_mock import mocker
 
 current_dir = os.path.dirname(__file__)
 
@@ -41,3 +43,26 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     calculate(args.input_file, args.output_file)
+
+
+def test_take_from_list():
+    assert take_from_list([0, 1], 0) == [0]
+    assert take_from_list([11, 12, 13, 14, 15], [1, 3, 5-1]) == [12, 14, 15]
+    with pytest.raises(ValueError):
+        take_from_list([3, 4, 0, 1, 2], [0, 1, 0.5, 3, "four"])
+    with pytest.raises(IndexError):
+        take_from_list([3, 4, 0], [0, 1, 2, 6])
+
+def test_calculate(mocker):
+    #create a json file with the result by hand
+    test_file = open("test.json", "w")
+    test_file.write(json.dumps([1, 9, 8, 7]))
+    test_file.close()
+
+    #get a json file with the same result from the function -> :(
+    mocker.patch(__name__ + '.take_from_list', return_value = [1, 9, 8, 7])
+    calculate(args.input_file, args.output_file)
+    out_file = open("output.json", "r")
+
+    #they should be identical
+    assert test_file == out_file
